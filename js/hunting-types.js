7185076dab428031519e612e1283d5d5e087ab34
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-03-09 04:19:46 +0500

    Fixed toggle hubting item

--- js/hunting-types.js
+++ js/hunting-types.js
@@ -1,12 +1,22 @@
 (function () {
     const huntBlocks = document.querySelectorAll('.hunt__item');
+    let showCell = null;
+
     huntBlocks.forEach((item) => {
-        item.addEventListener('click', function () {
-            let isActive = this.classList.contains('hunt__item_active');
-            huntBlocks.forEach((item) => {
-                item.classList.remove('hunt__item_active');
-            });
-            if (!isActive) item.classList.add('hunt__item_active');
+        item.addEventListener('click', function (e) {
+            const {currentTarget} = e;
+
+            if (showCell != currentTarget) {
+                let isActive = this.classList.contains('hunt__item_active');
+
+                huntBlocks.forEach((item) => {
+                    item.classList.remove('hunt__item_active');
+                });
+
+                if (!isActive) item.classList.add('hunt__item_active');
+            }
+
+            showCell = currentTarget;
         });
     });
 })();
